3bd4055003fca6b97d6ea702aafb976dec5a77d3
skorodenko <mskorodenko@gmail.com>
2025-05-10 15:38:18 +0300

    Theme update

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -10,17 +10,17 @@ return {
         -- Color table for highlights
         -- stylua: ignore
         local colors = {
-            bg       = "#202328",
-            fg       = "#bbc2cf",
-            yellow   = "#ECBE7B",
-            cyan     = "#008080",
-            darkblue = "#081633",
-            green    = "#98be65",
-            orange   = "#FF8800",
-            violet   = "#a9a1e1",
-            magenta  = "#c678dd",
-            blue     = "#51afef",
-            red      = "#ec5f67",
+            bg       = "#181616",
+            fg       = "#c5c9c5",
+            yellow   = "#e6c384",
+            cyan     = "#7aa89f",
+            darkblue = "#7fb4ca",
+            green    = "#87a987",
+            orange   = "#e6c384",
+            violet   = "#938aa9",
+            magenta  = "#a292a3",
+            blue     = "#8ba4b0",
+            red      = "#e46876",
         }
 
         local conditions = {
