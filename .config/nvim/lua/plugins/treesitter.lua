9c8de22eb88dbbda1c2a95f1a5985eb64311d715
skorodenko <mskorodenko@gmail.com>
2025-07-10 19:23:45 +0300

    ...

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,13 @@
 return {
-    { 
-        "nvim-treesitter/nvim-treesitter", 
+    {
+        "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         config = function()
             local configs = require("nvim-treesitter.configs")
 
             configs.setup({
-      	        auto_install = false,
-                -- ensure_installed = { "lua", "vim", "vimdoc", "c", "python", "rust" },
-                ensure_installed = {},
-                sync_install = false,
+                auto_install = true,
+                ensure_installed = { "lua", "vim", "vimdoc", "c", "python", "rust" },
                 highlight = { enable = true },
                 indent = { enable = true },
             })
